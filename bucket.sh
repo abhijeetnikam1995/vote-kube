6973ae9fe416fa9455bf6a7f7dd1a1762c34179d
Abhijeet Nikam <35236714+abhijeetnikam1995@users.noreply.github.com>
2023-06-15 11:01:52 +0530

    Update bucket.sh

--- bucket.sh
+++ bucket.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+export AWS_DEFAULT_REGION=us-east-1
 aws s3api create-bucket --bucket abdnikam2
 aws s3api put-object --bucket abdnikam2 --key dev/eks-cluster/
 aws s3api put-object --bucket abdnikam2 --key dev/aws-lbc/
 aws s3api put-object --bucket abdnikam2 --key dev/aws-lbc-ingress/
+aws dynamodb create-table \
+    --table-name abdnikam-lbc \
+    --attribute-definitions \
+        AttributeName=LockID,AttributeType=S \
+    --key-schema \
+        AttributeName=LockID,KeyType=HASH \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+        
+aws dynamodb create-table \
+    --table-name abdnikam \
+    --attribute-definitions \
+        AttributeName=LockID,AttributeType=S \
+    --key-schema \
+        AttributeName=LockID,KeyType=HASH \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
